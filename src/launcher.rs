6627ea665c24a8183632aea3263ed6b950dc2c0c
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-08-13 14:19:52 +0200

    MVP

--- src/launcher.rs
+++ src/launcher.rs
@@ -3,13 +3,18 @@ use sdl2::{
     keyboard::{Keycode, TextInputUtil},
     pixels::Color,
     rect::Rect,
-    ttf, Sdl, VideoSubsystem,
+    render::Canvas,
+    ttf,
+    video::Window,
+    Sdl, VideoSubsystem,
 };
 
 pub struct Launcher {
     options: Vec<String>,
+    query: String,
     context: Sdl,
     video: VideoSubsystem,
+    canvas: Canvas<Window>,
     ttf: ttf::Sdl2TtfContext,
     text: TextInputUtil,
 }
@@ -17,49 +22,47 @@ pub struct Launcher {
 impl Launcher {
     pub fn new(options: Vec<impl ToString>) -> Self {
         let options = options.iter().map(|s| s.to_string()).collect();
+
         let context = sdl2::init().expect("failed to create sdl context");
+
         let video = context
             .video()
             .expect("failed to initialize sdl video subsystem");
-        let ttf = ttf::init().expect("failed to create sdl ttf context");
-        let text = video.text_input();
 
-        Self {
-            options,
-            context,
-            video,
-            ttf,
-            text,
-        }
-    }
-
-    pub fn launch(&self) -> String {
-        // NOTE: to render text follow this procedure:
-        // - load a font via Sdl2TtfContext
-        // - render text to a surface using Font::render
-        // - create a target rect (use Surface::width and Surface::height)
-        // - create a texture using TextureCreator
-        // - copy the texture into the target rect using Canvas::copy
-
-        // TODO: use `font_kit` crate to find font families by name
-        // https://docs.rs/font-kit/latest/font_kit/index.html
-        // https://docs.rs/font-kit/latest/font_kit/handle/enum.Handle.html
-
-        // TODO: better error handling
-        let window = self
-            .video
+        let window = video
             .window("", 1000, 700) // TODO: calculate dimensions or get them via config
             .position_centered()
             .borderless()
             .build()
             .expect("failed to create window");
 
-        let mut canvas = window
+        let canvas = window
             .into_canvas()
             .build()
             .expect("failed to create canvas");
 
-        let creator = canvas.texture_creator();
+        let ttf = ttf::init().expect("failed to create sdl ttf context");
+
+        let text = video.text_input();
+
+        Self {
+            options,
+            query: String::from(""),
+            context,
+            video,
+            canvas,
+            ttf,
+            text,
+        }
+    }
+
+    // TODO: better error handling
+    pub fn launch(&mut self) -> String {
+        let creator = self.canvas.texture_creator();
+
+        // TODO: use `font_kit` crate to find font families by name
+        // https://docs.rs/font-kit/latest/font_kit/index.html
+        // https://docs.rs/font-kit/latest/font_kit/handle/enum.Handle.html
 
         // TODO: make font configurable
         let font = self
@@ -67,50 +70,77 @@ impl Launcher {
             .load_font("/usr/share/fonts/TTF/IosevkaNerdFont-Regular.ttf", 16)
             .expect("failed to load font");
 
-
-        let surface = font
-            .render("Hello World!")
-            .solid(Color::WHITE)
-            .expect("failed to render text");
-
-        let text_rect = Rect::new(12, 12, surface.width(), surface.height());
-
-        let texture = creator
-            .create_texture_from_surface(&surface)
-            .expect("failed to create texture");
-
         // TODO: handle theming
-        canvas.set_draw_color(Color::BLACK);
-        canvas.clear();
-        canvas.present();
+        self.canvas.set_draw_color(Color::BLACK);
+        self.canvas.clear();
+        self.canvas.present();
 
         let mut event_pump = self
             .context
             .event_pump()
             .expect("failed to create event pump");
 
+        self.text.start();
         'main_loop: loop {
-            canvas.set_draw_color(Color::BLACK);
-            canvas.clear();
+            self.canvas.set_draw_color(Color::BLACK);
+            self.canvas.clear();
 
             for event in event_pump.poll_iter() {
+                // TODO: handle input
                 match event {
                     Event::Quit { .. }
                     | Event::KeyDown {
                         keycode: Some(Keycode::Escape),
                         ..
                     } => break 'main_loop,
+                    Event::KeyDown { keycode, .. } => {
+                        if let Some(key) = keycode {
+                            match key {
+                                Keycode::Backspace => {
+                                    if !self.query.is_empty() {
+                                        let _ = self.query.pop();
+                                    }
+                                }
+                                Keycode::Return => {
+                                    break 'main_loop
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    Event::TextInput { text, .. } => {
+                        self.query += &text;
+                    }
                     _ => {}
                 }
             }
 
-            // TODO: handle input
+            // NOTE: to render text follow this procedure:
+            // - load a font via Sdl2TtfContext
+            // - render text to a surface using Font::render
+            // - create a target rect (use Surface::width and Surface::height)
+            // - create a texture using TextureCreator
+            // - copy the texture into the target rect using Canvas::copy
 
-            let _ = canvas.copy(&texture, None, Some(text_rect));
+            if !self.query.is_empty() {
+                // `blended` means something like antialiasing
+                let surface = font
+                    .render(&self.query)
+                    .blended(Color::WHITE) // TODO: theming
+                    .expect("failed to render text");
 
-            canvas.present();
+                let text_rect = Rect::new(12, 12, surface.width(), surface.height());
+
+                let texture = creator
+                    .create_texture_from_surface(&surface)
+                    .expect("failed to create texture");
+
+                let _ = self.canvas.copy(&texture, None, Some(text_rect));
+            }
+
+            self.canvas.present();
         }
 
-        "".into()
+        self.query.clone()
     }
 }
